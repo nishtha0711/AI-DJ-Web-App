6d0445d5672f68b30af3f3612b52b1e966644b7f
nishtha0711 <105867986+nishtha0711@users.noreply.github.com>
2022-07-13 19:33:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
 
 function preload(){
     song = loadSound("music.mp3");
@@ -25,6 +26,17 @@ function modelLoaded() {
 
 function draw(){
     image(video, 0, 0, 600, 500);
+    fill("#FF0000");
+    stroke("#FF0000");
+    if(scoreLeftWrist > 0.2){
+
+       circle(leftWristX,leftWristY,20);
+       InNumberleftWristY = Number(leftWristY);
+       remove_decimals = floor(InNumberleftWristY);
+       volume = remove_decimals/500;
+       document.getElementById("volume").innerHTML ="Volume = " + volume;
+       song.setVolume(volume);
+    }
 }
 
 function play(){
@@ -36,6 +48,8 @@ function play(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
